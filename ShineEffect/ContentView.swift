e913f2ba0569aab0280b5e5c2291c7b8e932f150
shenxiang <shenxiang@foursmile001.com>
2024-01-11 17:28:06 +0800

    Initial Commit

---
+++ ShineEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShineEffect
+//
+//  Created by FS on 2024/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShineEffect/ShineEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShineEffectApp.swift
+//  ShineEffect
+//
+//  Created by FS on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct ShineEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
