2d9c6f648cae65e7af035545a5688ae06f498f86
Shen <863461783@qq.com>
2024-02-03 08:23:17 +0800

    优化

---
+++ ShineEffect/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  ShineEffect
+//
+//  Created by 沈翔 on 2024/2/2.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
